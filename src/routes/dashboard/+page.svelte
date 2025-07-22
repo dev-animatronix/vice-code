fba262690f94d91b01268b1eb328e81179d54f9c
animatronix <dev.animatronix@gmail.com>
2025-07-22 20:09:28 +0200

    initial commit

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -35,14 +35,31 @@
   }
 </script>
 
-{#if error}
-  <p class="text-red-600">{error}</p>
-{:else if projects && projects.projects}
-  <ul class="list bg-base-100 rounded-box shadow-md">
-    {#each projects.projects as project}
-      <Item {project} />
-    {/each}
-  </ul>
-{:else}
-  <span class="loading loading-spinner loading-lg"></span>
-{/if}
+<main class="min-h-screen flex justify-center pt-12">
+  <div class="card shadow-sm max-w-4xl w-full space-y-4">
+    <div class="flex justify-between items-center p-4">
+      <h2 class="text-xl font-semibold">
+        {#if projects?.projects?.length}
+          Vous avez {projects.projects.length} projet{projects.projects.length > 1 ? 's' : ''}
+        {:else}
+          Aucun projet
+        {/if}
+      </h2>
+      <button class="btn btn-primary">
+        + Créer un projet
+      </button>
+    </div>
+    {#if error}
+      <p class="text-red-600">{error}</p>
+    {:else if projects && projects.projects}
+      <ul class="list bg-base-100 rounded-box shadow-md">
+        {#each projects.projects as project}
+          <Item {project} />
+        {/each}
+      </ul>
+    {:else}
+      <span class="loading loading-spinner loading-lg m-4"></span>
+    {/if}
+  </div>
+</main>
+
--- src/routes/dashboard/Item.svelte
+++ src/routes/dashboard/Item.svelte
@@ -3,6 +3,7 @@
   import { DeleteProject } from '$lib/api';
   export let project;
   let projectToDelete = null;
+  let projectToRename = null;
   let deletemodal;
   let success = false;
   let error = false;
@@ -44,6 +45,7 @@
 {/if}
 <!-- svelte-ignore a11y_missing_attribute -->
 <!-- svelte-ignore a11y_consider_explicit_label -->
+<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
 
 <li class="list-row">
   <div class="text-lg align-middle">{project.name}</div>
@@ -60,9 +62,23 @@
   <button on:click={() => goto(`/project/${project.id}`)} class="btn btn-square btn-ghost">
     <svg class="size-[1.2em]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M20.8477 1.87868C19.6761 0.707109 17.7766 0.707105 16.605 1.87868L2.44744 16.0363C2.02864 16.4551 1.74317 16.9885 1.62702 17.5692L1.03995 20.5046C0.760062 21.904 1.9939 23.1379 3.39334 22.858L6.32868 22.2709C6.90945 22.1548 7.44285 21.8693 7.86165 21.4505L22.0192 7.29289C23.1908 6.12132 23.1908 4.22183 22.0192 3.05025L20.8477 1.87868ZM18.0192 3.29289C18.4098 2.90237 19.0429 2.90237 19.4335 3.29289L20.605 4.46447C20.9956 4.85499 20.9956 5.48815 20.605 5.87868L17.9334 8.55027L15.3477 5.96448L18.0192 3.29289ZM13.9334 7.3787L3.86165 17.4505C3.72205 17.5901 3.6269 17.7679 3.58818 17.9615L3.00111 20.8968L5.93645 20.3097C6.13004 20.271 6.30784 20.1759 6.44744 20.0363L16.5192 9.96448L13.9334 7.3787Z"  fill="currentColor" stroke="none"></path> </g></svg>
   </button>
-  <button on:click={() => { projectToDelete = project.id; deletemodal.showModal(); }} class="btn btn-square btn-soft btn-error">
-    <svg class="size-[1.2em]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-  </button>
+  <div class="dropdown dropdown-top dropdown-end">
+    <div tabindex="0" role="button" class="btn btn-square btn-ghost"><svg class="size-[1.2em]" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 12C9.10457 12 10 12.8954 10 14C10 15.1046 9.10457 16 8 16C6.89543 16 6 15.1046 6 14C6 12.8954 6.89543 12 8 12Z" fill="currentColor"></path> <path d="M8 6C9.10457 6 10 6.89543 10 8C10 9.10457 9.10457 10 8 10C6.89543 10 6 9.10457 6 8C6 6.89543 6.89543 6 8 6Z" fill="currentColor"></path> <path d="M10 2C10 0.89543 9.10457 -4.82823e-08 8 0C6.89543 4.82823e-08 6 0.895431 6 2C6 3.10457 6.89543 4 8 4C9.10457 4 10 3.10457 10 2Z" fill="currentColor"></path> </g></svg></div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <button on:click={() => { projectToRename = project.id; renamemodal.showModal(); }} class="btn btn-error">
+          <svg class="size-[1.2em]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+          supprimer
+        </button>
+      </li>
+      <li>
+        <button on:click={() => { projectToDelete = project.id; deletemodal.showModal(); }} class="btn btn-error">
+          <svg class="size-[1.2em]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+          supprimer
+        </button>
+      </li>
+    </ul>
+  </div>
 </li>
 
 <dialog bind:this={deletemodal} class="modal">
